5e11f54a8ee2beaec84d4e1614b6f7644cd21a23
Hunter Gerlach <HunterGerlach@users.noreply.github.com>
2023-03-01 00:08:53 -0500

    Updated my-app/src/test/java/com/mycompany/app/AppTest.java

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * 🤩 Unit test for simple App. 🤩 
+ * 🧪 Unit test for simple Application. 🤓 
  */
 public class AppTest 
     extends TestCase
