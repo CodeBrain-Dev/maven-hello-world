b05e63b96c09e25dfbcd15044d76f73310fabc9b
Hunter Gerlach <HunterGerlach@users.noreply.github.com>
2023-03-03 20:21:41 -0500

    Updated my-app/src/test/java/com/mycompany/app/AppTest.java

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -7,22 +7,15 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest  extends TestCase {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
+   /**  Create the test case for testing the application.  */  
+
+   public AppTest(String testName) {  super(testName); }
+
+   /** Return the suite of tests being tested. */
+
+   public static Test suite() {
         return new TestSuite( AppTest.class );
     }
 
