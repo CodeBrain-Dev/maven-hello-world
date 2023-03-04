5e9c4fc23a7e058a7202dd70931e122684b3f2f9
Hunter Gerlach <HunterGerlach@users.noreply.github.com>
2023-03-04 04:57:41 -0500

    Add Checker Framework installation instructions
                Body:
                Add instructions for Checker Framework installation to App.java to prevent NullPointerExceptions.

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,14 +8,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Welcome to the Maven Hello World application!");
+        System.out.println( "Hello World!" );
 
         System.out.println("A NullPointerException can be a real pain...");
         Object myObject = null;
         /**
          * Checker prevents this from compiling...
          */
-//        System.out.println("myObject: " + myObject.toString());
+//        if (myObject != null) {
+    System.out.println("myObject: " + myObject.toString());
+}
         /**
          * ... which is simply fantastic. It shows:
          *
