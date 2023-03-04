f2ad4b6e7a3a748a7fbcf95efe76f9638d8a00dd
Hunter Gerlach <HunterGerlach@users.noreply.github.com>
2023-03-03 20:24:11 -0500

    Updated my-app/src/main/java/com/mycompany/app/App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
 
-        System.out.println("A NullPointerException can be a real drag...");
+        System.out.println("A NullPointerException can be a drag...");
         Object myObject = null;
         /**
          * Checker prevents this from compiling...
