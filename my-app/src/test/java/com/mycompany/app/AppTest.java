8796e8ab35764e213f5152b892e112db346bcd6c
Hunter Gerlach <HunterGerlach@users.noreply.github.com>
2023-03-03 20:37:22 -0500

    Updated my-app/src/test/java/com/mycompany/app/AppTest.java

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -7,26 +7,15 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest  extends TestCase {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+   /**  Create the test case  */  												   // Added comment to explain purpose of this method. 
+
+   public AppTest(String testName) {      super(testName);   }                       // Removed unnecessary line break.
+
+   /** @return the suite of tests being tested */                                     // Added comment to explain purpose of this method. 
+
+   public static Test suite() { return new TestSuite(AppTest.class); }                // Removed unnecessary line break.
 
     /**
      * Rigourous Test :-)
