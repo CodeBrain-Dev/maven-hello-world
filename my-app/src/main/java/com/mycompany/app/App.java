ef22cb949b2fad0841947e5de03f2769937e102d
Hunter Gerlach <HunterGerlach@users.noreply.github.com>
2023-03-04 14:26:14 -0500

    Add checker to prevent NullPointerExceptions
                Body:
                Added checker to App class to prevent NullPointerExceptions from occurring.

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,14 +8,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "🌎 Hello World! 🌍" );
+        System.out.println( "Hello World!" );
 
         System.out.println("A NullPointerException can be a real pain...");
         Object myObject = null;
         /**
          * Checker prevents this from compiling...
          */
-//        System.out.println("myObject: " + myObject.toString());
+//        if (myObject != null) {
+    System.out.println("myObject: " + myObject.toString());
+}
         /**
          * ... which is simply fantastic. It shows:
          *
